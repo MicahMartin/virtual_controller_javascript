9b78ebf3c14dccf94284cfb9c504492603903a1c
MicahMartin <micah.m329@gmail.com>
2023-09-13 18:35:37 -0400

    init commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import CircularBuffer from "./src/util/CircularBuffer";
+
+console.log("Hello via Bun!");
+const inputBuffer = new CircularBuffer(60);
---
+++ src/util/CircularBuffer.js
@@ -0,0 +1,18 @@
+export default class CircularBuffer {
+  #capacity = 0;
+  #buffer = [];
+
+  constructor(capacity){
+    this.#capacity = capacity;
+  }
+
+  enq(item){
+  }
+
+  deq(){
+  }
+
+  peek(index){
+    return buffer[index];
+  }
+}
